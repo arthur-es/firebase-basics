1a64b7c6d9f6e7278f306b6e7f64feb102074468
Arthur Castro <arthurcastroes@gmail.com>
2020-08-20 21:26:26 -0300

    funcoes

---
+++ js/utils.js
@@ -0,0 +1,32 @@
+//Referências
+const authForm = document.getElementById("authForm");
+const authFormTitle = document.getElementById("authFormTitle");
+const register = document.getElementById("register");
+const access = document.getElementById("access");
+
+function showItem(element){
+    element.style.display = 'block';
+}
+
+function hideItem(element){
+    element.style.display = 'none';
+}
+
+function toggleToRegister(){
+    authForm.submitAuthForm.innerHTML = 'Cadastrar conta';
+    authFormTitle.innerHTML = "Insira seus dados para se cadastrar";
+
+    hideItem(register);
+    showItem(access);
+}
+
+function toggleToAccess(){
+    authForm.submitAuthForm.innerHTML = 'Acessar';
+    authFormTitle.innerHTML = "Acesse a sua conta para continuar";
+
+    hideItem(access);
+    showItem(register);
+}
+
+
+
